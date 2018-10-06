2017624e6bd637931458c8506200d68621165529
luigisenzamici <luigisenzamici@outlook.it>
2018-10-06 12:13:31 +0200

    initial commit

---
+++ lib/ts-reflection.d.ts
@@ -0,0 +1,3 @@
+export declare function getMethods(obj: object): Array<string>;
+export declare function getProperties(obj: object): Array<string>;
+export declare function getPropertyByValue(obj: object, value: any): string;
---
+++ ts-reflection.ts
@@ -0,0 +1,27 @@
+﻿export function getMethods(obj: object): Array<string> {
+        let res: Array<string> = [];
+        let props = Object.getOwnPropertyNames(obj);
+        res = props.filter(function (property) {
+            return typeof obj[property] == 'function';
+        });
+        return res;
+    }
+export function getProperties(obj: object): Array<string>{
+        let res: Array<string> = [];
+        let props = Object.getOwnPropertyNames(obj);
+        res = props.filter(function (property) {
+            return typeof obj[property] != 'function';
+        });
+        return res;
+    }
+export function getPropertyByValue(obj: object, value: any): string{
+        let res: string;
+
+        let lsP = getProperties(obj);
+        res = lsP.filter(e => {
+            return obj[e] == value;
+        })[0];
+
+        return res;
+    }
+
