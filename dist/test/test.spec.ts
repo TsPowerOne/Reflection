c7dca11aef6835ec7ae815cc4f5f9d76ec296c49
luigisenzamici <luigisenzamici@outlook.it>
2018-10-06 18:27:35 +0200

    test added

--- dist/test/test.spec.ts
+++ dist/test/test.spec.ts
@@ -1,11 +1,20 @@
 import {getProperties} from '../src/reflection';
 
 import { expect } from 'chai';
+import { getPropertyByValue } from '../../reflection';
 describe('getProperties', () => {
 
   it('should return ciao', () => {
     let obj = {ciao:"ciao"};
     const result = getProperties(obj);
+
+    expect(result[0]).to.equal("ciao");
+
+  });
+  
+  it('should return ciao', () => {
+    let obj = {ciao:55};
+    const result = getPropertyByValue(obj, 55);
 
     expect(result[0]).to.equal("ciao");
 
